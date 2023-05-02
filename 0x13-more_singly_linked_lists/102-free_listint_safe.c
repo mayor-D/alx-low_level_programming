da4a20162abc34e762babefabeac2ef2a93f6d31
Egbayelo Mayowa <126684246+mayor-D@users.noreply.github.com>
2023-05-02 16:53:53 +0100

    Update 102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,8 +1,14 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- * free_listint_safe - frees a linked list project
- * */
+ * free_listint_safe - frees a linked list
+ * @h: pointer to the first node in the linked list
+ *
+ * Return: number of elements in the freed list
+ */
+ /*
+*project
+*/
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
