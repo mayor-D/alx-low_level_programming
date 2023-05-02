e97d0a86af842411d5d4f5a263de09389d148aab
Egbayelo Mayowa <126684246+mayor-D@users.noreply.github.com>
2023-05-02 16:53:06 +0100

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,8 +1,14 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked project
- * */
+ * print_listint_safe - prints a linked list, safely
+ * @head: list of type listint_t to print
+ *
+ * Return: number of nodes in the list
+ */
+ /*
+*project
+*/
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
